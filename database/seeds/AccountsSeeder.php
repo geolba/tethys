87fff275274b6e9cffc5f35fddc96c1ee6318c40
Arno Kaimbacher <arno.kaimbacher@outlook.at>
2020-07-28 20:29:03 +0200

    - npm updates

--- database/seeds/AccountsSeeder.php
+++ database/seeds/AccountsSeeder.php
@@ -1,46 +1,39 @@
 <?php
 
 use Carbon\Carbon;
-use Database\DisableForeignKeys;
-use Database\TruncateTable;
+// use Database\DisableForeignKeys;
+// use Database\TruncateTable;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-use Faker\Factory as Faker;
 
 class AccountsTableSeeder extends Seeder
 {
     public function run()
     {
-        // DB::table('users')->delete();
-
-        // User::create([
-        // 'name' => str_random(10),
-        // 'email' => 'foo@gmail.com',
-        // 'password' => bcrypt('secret')
-        // ]);
+      
         DB::table('accounts')->insert([
             [
                 'login' => "admin",
-                'email' => "repository" . '@geologie.ac.at',
-                'password' => bcrypt('admin007'),
+                'email' => "admin@localhost",
+                'password' => bcrypt('secret'),
                 'created_at' => Carbon::now(),
             ],
             [
                 'login' => "Submitty",
-                'email' => "submitter@geologie.ac.at",
-                'password' => bcrypt('rdr007'),
+                'email' => "submitter@localhost",
+                'password' => bcrypt('secret'),
                 'created_at' => Carbon::now(),
             ],
             [
                 'login' => "Eddy",
-                'email' => "editor@geologie.ac.at",
-                'password' => bcrypt('rdr007'),
+                'email' => "editor@localhost",
+                'password' => bcrypt('secret'),
                 'created_at' => Carbon::now(),
             ],
             [
                 'login' => "Review",
-                'email' => "review@geologie.ac.at",
-                'password' => bcrypt('rdr007'),
+                'email' => "review@localhost",
+                'password' => bcrypt('secret'),
                 'created_at' => Carbon::now(),
             ],
         ]);
